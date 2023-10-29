7724ecc4db8bf1e1af1865ba9d8880427e4d5015
Roop2610 <149135498+Roop2610@users.noreply.github.com>
2023-10-29 12:14:42 +0530

    Data Science Ecosystem exercise notebook file

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+# 
+
+# ## In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# ### Some of the popular languages that Data Scientists use are:
+# 
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Java
+# 5. Julia
+# 6. Scala
+
+# ## Some of the commonly used libraries used by Data Scientists include:
+# 1. matplotlib
+# 2. panda
+# 3. numpy
+# 4. ggplot2
+# 5. keras
+# 6. TensorFlow
+
+# ## data science tools
+# | Data Science tools |
+# | --- |
+# | Apache Spark |
+# | R Studio |
+# | Sensor Flow |
+# | Apache Hadoop |
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+
+# In[1]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[4]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+Hours = 200/60
+
+print("200 minutes into hours are", Hours)
+
+
+# ## objectives of this notebook file
+# **Objectives:**
+# - Tools for data science
+# - Arathmatic using python
+# - Markdown excecise
+# - Data Science libraries
+# - Data science languages
+
+# ## Author
+# Rupesh Sangar
+
+# In[ ]:
+
+
+
+
